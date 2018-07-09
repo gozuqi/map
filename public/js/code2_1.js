32c7660aff6e6f35281d2cbf177b0a1fa00d54ac
go suzuki <gosuzuki@MMadminnoMacBook-puro-3.local>
2018-07-09 16:43:39 +0900

    commit

--- public/js/code2_1.js
+++
@@ -1,9 +0,0 @@
-function initialize() {
-      if (GBrowserIsCompatible()) {
-        var map = new GMap2(document.getElementById("map_canvas"));
-        map.setCenter(new GLatLng(35.681379,139.765577), 13);
-        var directions = new GDirections(map, document.getElementById('route'));
-        directions.load('from: 東京駅 to: 渋谷駅', {locale: 'ja_JP'});
-      }
-    }
-
